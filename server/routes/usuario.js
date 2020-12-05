a4efac766c92215db2275355e73fe367f5705c9d
Floren <florenga@gmail.com>
2020-12-05 12:00:48 +0100

    Actualizado el PUT de usuario

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -3,7 +3,7 @@ const Usuario = require('../models/usuario');
 
 // Encriptar la contraseña
 const bcrypt = require('bcrypt');
-
+const _ = require('underscore');
 const app = express();
 
 const saltRounds = 10;
@@ -41,9 +41,30 @@ app.post('/usuario', (req, res) => {
 });
 
 app.put('/usuario/:id', function(req, res) {
-    res.json('put Usuario');
-});
+    let id = req.params.id;
 
+    // Se usa la librería underscore, con el método pick para filtrar qué campos queremos usar (el resto no entrarían);
+    //  El primer parámetro sería el body, con TODOS los campos, y como segundo parámetro se le pasa un array con los campos que se quieran actualizar.
+    let body = _.pick(req.body, ['nombre', 'emil', 'img', , 'role', 'estado']);
+
+    // Encontrar el usuario
+    // En el tercer parámetro podemos añadir opciones;
+    //   new -> devuelve la información ctualizada del usuario; si no ponemos esta opción, usuarioDB contendría los valores    previos a la actualización
+    //  runValidators: aplica las validaciones definidas en el Schema; en este caso, al hacer una put podríamos pasar un role que no esté entre los definidos en la enumeración: podrían poner cualquier valor y "tragaría"
+    Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        })
+    });
+});
 app.delete('/usuario', function(req, res) {
     res.json('delete Usuario');
 });
