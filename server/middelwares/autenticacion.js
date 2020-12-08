42c421c055d0014cf01437e2a6642a9494de172d
Floren <florenga@gmail.com>
2020-12-08 16:51:23 +0100

    Verificación token para imágenes

--- server/middelwares/autenticacion.js
+++ server/middelwares/autenticacion.js
@@ -45,7 +45,43 @@ let verificaAdminRole = (req, res, next) => {
 };
 
 
+
+// ====================================================
+// Verificar token para IMG
+// ====================================================
+
+let verificaTokenImg = (req, res, next) => {
+    let token = req.query.token;
+
+    if (!token) {
+        return res.status(400).json({
+            ok: false,
+            err: {
+                message: "No tiene permiso para ver el recurso"
+            }
+        })
+    }
+
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                err: {
+                    message: 'Token no válido.'
+                }
+            });
+        }
+
+        // Esta función devuelve los datos del usuario logueado
+        req.usuario = decoded.usuario;
+        next();
+    });
+
+}
+
+
 module.exports = {
     verificaToken,
-    verificaAdminRole
+    verificaAdminRole,
+    verificaTokenImg
 }
